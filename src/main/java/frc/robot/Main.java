ab35824780d25c0aceb06b04b5b05233355f1ddd
burpotato <dezman18@outlook.com>
2022-03-18 17:34:54 -0500

    autofetch?

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -17,6 +17,10 @@ public final class Main {
   /**
    * Main initialization function. Do not perform any initialization here.
    *
+   * i'll do what i want you bozo
+   * 
+   * I'M the programmer here
+   * 
    * <p>If you change your main robot class, change the parameter type.
    */
   public static void main(String... args) {
